51b5fae39ac6d6f209fb704a1f34c9dcfef9f447
poylib <poy_lib@naver.com>
2022-08-29 16:35:36 +0900

    [ADD] init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Router from './pages/Router';
 
 import './styles/reset.scss';
 import './styles/common.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Router />);
