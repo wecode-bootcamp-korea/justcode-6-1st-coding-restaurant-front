513353ddf31efff764613fe3fd82bdd78fee6a50
Chooteacher <zmz14s@gmail.com>
2022-08-29 23:30:14 +0900

    마이페이지 레이아웃 구현

--- LeeEunJi/src/pages/Router.js
+++ LeeEunJi/src/pages/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
 import Login from './Login/Login';
+import Mypage from './Mypage/Mypage';
 import Signup from './Signup/Signup';
 
 import Mypage from './Mypage/Mypage';
