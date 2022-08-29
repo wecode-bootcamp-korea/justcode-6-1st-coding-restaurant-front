29978edf08697c25446baf20858a08a30f9a311a
Chooteacher <zmz14s@gmail.com>
2022-08-29 23:30:14 +0900

    마이페이지 레이아웃 구현

--- LeeEunJi/src/index.js
+++ LeeEunJi/src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 
 import './styles/reset.scss';
 import './styles/common.scss';
+import Router from './pages/Router';
+import Login from './pages/Login/Login';
+import Mypage from './pages/Mypage/Mypage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Router />);
--- LeeEunJi/src/pages/Router.js
+++ LeeEunJi/src/pages/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './Home/Home';
 import Login from './Login/Login';
+import Mypage from './Mypage/Mypage';
 import Signup from './Signup/Signup';
 
 function Router() {
@@ -12,6 +13,7 @@ function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/mypage" element={<Mypage />} />
       </Routes>
     </BrowserRouter>
   );
