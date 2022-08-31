b6f95c4badba5df3963d1b0b242530667aa86de5
Chooteacher <zmz14s@gmail.com>
2022-08-31 18:21:43 +0900

    미식평 작업중

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -5,6 +5,7 @@ import Login from './Login/Login';
 import Mypage from './Mypage/Mypage';
 import Signup from './Signup/Signup';
 import Orderlist from './Mypage/Orderlist';
+import Review from './Mypage/Review';
 
 function Router() {
   return (
@@ -15,6 +16,7 @@ function Router() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/orderlist" element={<Orderlist />} />
+        <Route path="/review" element={<Review />} />
       </Routes>
     </BrowserRouter>
   );
