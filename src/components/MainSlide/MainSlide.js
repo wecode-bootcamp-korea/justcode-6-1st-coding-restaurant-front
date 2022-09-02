6f7dcf7cf6f2e0faca1a0db24016944b36be9c28
YesunPark <lynn08082@gmail.com>
2022-09-02 22:07:30 +0900

    [Add]: 메인페이지 큰 슬라이더 UI 구현

--- src/components/MainSlide/MainSlide.js
+++ src/components/MainSlide/MainSlide.js
@@ -2,14 +2,39 @@ import React, { useState, useEffect } from 'react';
 
 import css from './MainSlide.module.scss';
 
-// 1. 슬라이드 이미지 넣기ㅇ
-// 2. 버튼 구현ㅇ
-// 3. 버튼 누르면 오른쪽 왼쪽 이미지 위치 바뀜
-// 4. 이미지 부드럽게 바뀌기
-// 5. 이미지 바뀌면 우측아래 숫자 바뀌게
+// 슬라이드 이미지 넣기ㅇ
+// 버튼 구현ㅇ
+// 버튼 누르면 오른쪽 왼쪽 이미지 위치 바뀜
+// 맨끝으로 오면 반대편 끝으로 이어지게
+// 이미지 부드럽게 바뀌기
+// 이미지 바뀌면 우측아래 숫자 바뀌게
 
 function MainSlide() {
   const [imgData, setImgData] = useState([]);
+  const [imgLocation, setImgLocation] = useState(0);
+  const [slideIdx, setSlideIdx] = useState(0);
+  const slideLen = imgData.length; //5
+
+  const rightBtnClick = () => {
+    if (slideIdx < slideLen - 1) {
+      setImgLocation(imgLocation + 1);
+      setSlideIdx(slideIdx + 1);
+    } else {
+      setImgLocation(slideLen - 1);
+      setSlideIdx(slideIdx);
+    }
+  };
+
+  const leftBtnClick = () => {
+    if (slideIdx >= 1) {
+      setImgLocation(imgLocation - 1);
+      setSlideIdx(slideIdx - 1);
+    } else {
+      console.log('slideIdx :', slideIdx);
+      setImgLocation(0);
+      setSlideIdx(0);
+    }
+  };
 
   useEffect(() => {
     fetch('/data/mainSlide.json')
@@ -22,25 +47,55 @@ function MainSlide() {
   return (
     <div className={css.MainSlide}>
       <div className={css['img-slide']}>
-        {imgData.map(item => {
-          return (
-            <div key={item.id}>
-              <img src={item.url} />
-            </div>
-          );
-        })}
+        <div
+          className={css['img-slide-box']}
+          style={{
+            transform: `translate3d(${imgLocation * -100 + 'vw'},0px,0px)`,
+            transition: '450ms',
+          }}
+        >
+          {imgData.map(item => {
+            return (
+              <div key={item.id}>
+                <img src={item.url} />
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div className={css['slide-content']}>
+        <div
+          className={css['description-box']}
+          style={{
+            transform: `translate3d(${imgLocation * -100 + 'vw'},0px,0px)`,
+            transition: '450ms',
+            width: `${slideLen * 100}vw`,
+          }}
+        >
+          {imgData.map(item => {
+            return (
+              <div className={css.description} key={item.id}>
+                <div>{item.name}</div>
+                <div className={css.detail}>{item.description}</div>
+              </div>
+            );
+          })}
+        </div>
         <button
+          onClick={leftBtnClick}
           className={`
             ${css['button']}
           ${css['button-left']}`}
         />
-        <div className={css.description}>
-          상품이름 <br />
-          설명
+        <button
+          onClick={rightBtnClick}
+          className={`
+            ${css['button']}
+          ${css['button-right']}`}
+        />
+        <div className={css['slide-idx']}>
+          {slideIdx + 1} / {slideLen}
         </div>
-        <button className={css['button']} />
       </div>
     </div>
   );
