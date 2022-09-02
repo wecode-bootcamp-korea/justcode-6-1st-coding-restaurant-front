fb2d94e5cbe16eb1f86477006ef993b3bb17548d
Roxy100 <misoamy16@gmail.com>
2022-09-02 12:39:41 +0900

    [Refactor]: css파일 공백없애기 및 image 폴더이름변경

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -97,19 +97,19 @@ function Signup() {
           <div className={css.divider}></div>
           <div className={css['social-account']}>
             <div className={css['social-account-facebook']}>
-              <img alt="facebook" src="./images/facebook.png" width="50px" />
+              <img alt="facebook" src="./image/signUpLogin/facebook.png" />
               <span>페이스북 계정으로 가입하기</span>
             </div>
             <div className={css['social-account-naver']}>
-              <img alt="naver" src="./images/naver.png" width="50px" />
+              <img alt="naver" src="./image/signUpLogin/naver.png" />
               <span>네이버 계정으로 가입하기</span>
             </div>
             <div className={css['social-account-kakao']}>
-              <img alt="kakao" src="./images/kakao.png" width="50px" />
+              <img alt="kakao" src="./image/signUpLogin/kakao.png" />
               <span>카카오 계정으로 가입하기</span>
             </div>
             <div className={css['social-account-apple']}>
-              <img alt="apple" src="./images/apple.png" width="50px" />
+              <img alt="apple" src="./image/signUpLogin/apple.png" />
               <span>Apple로 로그인</span>
             </div>
           </div>
