abd6416d3b3d735558069c919de4e0ea87b31f36
YesunPark <lynn08082@gmail.com>
2022-09-03 18:32:59 +0900

    [Refactor]: 상세페이지 설명/ 주문 컴포넌트화

---
+++ src/components/Detail/DetailMain.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const DetailMain = () => {};
+
+export default DetailMain;
--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
+import DetailMain from '../../components/Detail/DetailMain';
+import DetailSub from '../../components/Detail/DetailSub';
+
 import css from './Detail.module.scss';
+
+// 1. 상세페이지 불러오기 API : 제목, 부제목, 상세정보, 리뷰,
+//   번들, 가격, 배송비, 도착일
+// 2. 장바구니 담기 API
 
 const Detail = () => {
   return (
     <div className={css.detail}>
-      <div className={css['detail-container']}>
-        <div className={css.container}>
-          <div className={css.main}></div>
-          <div className={css.sub}></div>
+      <div className={css.container}>
+        <div className={css.main}>
+          <DetailMain />
+        </div>
+        <div className={css.sub}>
+          <DetailSub />
         </div>
       </div>
     </div>
