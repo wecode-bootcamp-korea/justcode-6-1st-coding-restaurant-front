894166a70022498bcd9051088bd07d832a821725
YesunPark <108933466+YesunPark@users.noreply.github.com>
2022-09-03 11:15:13 +0900

    예선) [Design]: root 높이 지정해서 배경색적용 안되는 문제 해결, 헤더 높이설정 해제 (#14)
    * [Design]: #root 크기꽉차게 조정, 헤더 높이설정 해제

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-import Router from './pages/Router';
 
+import Router from './pages/Router';
 import './styles/reset.scss';
 import './styles/common.scss';
 import Login from './pages/Login/Login';
