5df3e3614f438360a2f85a41ebe0402d3344c4e0
YesunPark <lynn08082@gmail.com>
2022-09-04 20:01:38 +0900

    [Refactor]: 작업용 주석제거

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -6,17 +6,11 @@ import DetailSub from '../../components/Detail/DetailSub';
 
 import css from './Detail.module.scss';
 
-// 1. 상세페이지 불러오기 API : 제목, 부제목, 상세정보, 리뷰,
-//   번들, 가격, 배송비, 도착일
-// 2. 장바구니 담기 API
-
 const Detail = () => {
   const params = useParams();
   const [data, setData] = useState({});
   const { name, description, content, bundle, review, fixedprice, image } =
     data;
-
-  // <<<<<< 임시 api >>>>>>
 
   useEffect(() => {
     fetch(`http://localhost:8000/product/${params.id}`, {
@@ -30,16 +24,6 @@ const Detail = () => {
         setData(req.data[0]);
       });
   }, []);
-
-  //목데이터 사용해 fetch
-  // useEffect(() => {
-  //   fetch('/data/detail/detail.json')
-  //     .then(res => res.json())
-  //     .then(req => {
-  //       setData(req.data[0]);
-  //       console.log(req.data[0]);
-  //     });
-  // }, []);
 
   return (
     <div className={css.detail}>
