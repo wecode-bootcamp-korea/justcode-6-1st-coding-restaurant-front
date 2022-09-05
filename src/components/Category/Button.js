9b40e6e4abc30041a6a9c1e0801cb15fca8b8194
poylib <poy_lib@naver.com>
2022-09-05 13:45:12 +0900

    [Refactor]: bug fix

--- src/components/Category/Button.js
+++ src/components/Category/Button.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import css from './Button.module.scss';
 
@@ -35,41 +34,3 @@ function Button(props) {
   );
 }
 export default Button;
-=======
-import React, { useState } from 'react';
-import css from './Button.module.scss';
-
-function Button(props) {
-  const { handleBtn } = props;
-  const [check, setCheck] = useState([true, false, false, false, false]);
-  const onClick = e => {
-    const sortedPage = e.target.value;
-    handleBtn(sortedPage);
-  };
-  return (
-    <>
-      <div className={css['menu-button']}>
-        <button onClick={onClick} value="viewCount">
-          조회순
-        </button>
-      </div>
-      <div className={css['menu-button']}>
-        <button onClick={onClick} value="orderCount">
-          주문순
-        </button>
-      </div>
-      <div className={css['menu-button']}>
-        <button onClick={onClick} value="lowPrice">
-          낮은 가격순
-        </button>
-      </div>
-      <div className={css['menu-button']}>
-        <button onClick={onClick} value="highPrice">
-          높은 가격순
-        </button>
-      </div>
-    </>
-  );
-}
-export default Button;
->>>>>>> main
