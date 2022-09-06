c6ea476ad1837fa07b63a02d6302b7b901de4aa5
poylib <poy_lib@naver.com>
2022-09-06 10:49:10 +0900

    [Fix]: 마이페이지 리뷰 컴포넌트 분리 및 리뷰 삭제 기능 구현 (#34)
    * [Refactor]: 리뷰코드 컴포넌트화해서 map으로 사용할수있게 함
    
    * [Design]: 리뷰박스 플랙스 적용
    
    * [Desgin]: 이미지 규격 설정
    
    * [Add]: 상세페이지 라우팅 추가

--- src/components/Mypage/Review.js
+++ src/components/Mypage/Review.js
@@ -1,12 +1,9 @@
 import orderReviewCss from '../Mypage/Review.module.scss';
 import css from '../../pages/Mypage/Mypage.module.scss';
 import { useState, useEffect } from 'react';
-import ReviewModal from './ReviewModal';
-import ReviewModalRevise from './ReviewModalRevise';
+import ReviewList from './ReviewList';
 
 function Review() {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [ReviewModalReviseOpen, setReviewModalReviseOpen] = useState(false);
   const [myReview, setMyReview] = useState([]);
 
   useEffect(() => {
@@ -15,75 +12,29 @@ function Review() {
       .then(data => setMyReview(data.data));
   }, []);
 
-  const showModal = () => {
-    setModalOpen(true);
-  };
-
-  const reviseModal = () => {
-    setReviewModalReviseOpen(true);
-  };
   return (
     <div>
-      <div className={css.sectionTitle}>
-        <span>미식평</span>
-      </div>
-      <table className={orderReviewCss.reviewList}>
-        <tbody>
-          {myReview.map(myReviewItem => {
-            return (
-              <tr key={myReviewItem.id}>
-                <td>
-                  <img
-                    className={orderReviewCss.foodImg}
-                    src={myReviewItem.image_thumbnail}
-                  ></img>
-                </td>
-                <td className={orderReviewCss.tdSize}>
-                  <span className={orderReviewCss.order}>
-                    {myReviewItem.name}
-                  </span>
-                </td>
-                <td className={orderReviewCss.buttonId}>
-                  <div>
-                    <input
-                      type="hidden"
-                      value={myReviewItem.product_id}
-                    ></input>
-                    {myReviewItem.isWriting ? (
-                      <>
-                        <button
-                          className={orderReviewCss.button}
-                          onClick={reviseModal}
-                        >
-                          수정
-                        </button>
-                        <button className={orderReviewCss.button}>삭제</button>
-                      </>
-                    ) : (
-                      <button
-                        className={orderReviewCss.button}
-                        onClick={showModal}
-                      >
-                        등록
-                      </button>
-                    )}
-                    {modalOpen && <ReviewModal setModalOpen={setModalOpen} />}
-                    {ReviewModalReviseOpen && (
-                      <ReviewModalRevise
-                        setReviewModalReviseOpen={setReviewModalReviseOpen}
-                      />
-                    )}
-                  </div>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-
-      <div className={css.rowList}>
-        <p>조회 가능한 미식평이 없습니다.</p>
-      </div>
+      {myReview.length === 0 ? (
+        <div className={css.rowList}>
+          <p>조회 가능한 미식평이 없습니다.</p>
+        </div>
+      ) : (
+        <>
+          <div className={css.sectionTitle}>
+            <span>미식평</span>
+          </div>
+          <div className={orderReviewCss.reviewList}>
+            {myReview.map(el => (
+              <ReviewList
+                key={el.id}
+                list={el}
+                removeBtn={myReview}
+                setRemoveBtn={setMyReview}
+              />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
