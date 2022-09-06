6eb9283a6c6effb044761b8b69caa6ef6d19b46a
poylib <poy_lib@naver.com>
2022-09-06 10:49:25 +0900

    [Fix]: 로그인시 로그인 버튼 마이페이지로 바꾸기 (#35)

--- src/components/Header/Login.js
+++ src/components/Header/Login.js
@@ -1,11 +1,26 @@
+import React, { useState } from 'react';
 import css from './Login.module.scss';
 import { Link } from 'react-router-dom';
 function Login() {
+  const [token, setToken] = useState(false);
+  const logout = () => {
+    localStorage.removeItem('token');
+    token ? setToken(false) : setToken(true);
+  };
   return (
     <div className={css['login-box']}>
       <span>고객센터</span>
-      <Link to="/login">로그인</Link>
-      <Link to="/signup">회원가입</Link>
+      {token ? (
+        <>
+          <Link to="/mypage">마이페이지</Link>
+          <span onClick={logout}>로그아웃</span>
+        </>
+      ) : (
+        <>
+          <Link to="/login">로그인</Link>
+          <Link to="/signup">회원가입</Link>
+        </>
+      )}
     </div>
   );
 }
