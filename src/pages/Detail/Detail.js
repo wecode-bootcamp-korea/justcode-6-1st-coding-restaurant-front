159ce1e1addae88dcc2a3c47857905ae4013e88c
YesunPark <lynn08082@gmail.com>
2022-09-07 22:53:46 +0900

    [Fix]: 헤더에 장바구니 수량 적용 중간저장

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -12,26 +12,26 @@ const Detail = () => {
   const { name, description, content, bundles, reviews, fixedprice, images } =
     data;
 
-  useEffect(() => {
-    fetch(`http://localhost:8000/product/${params.id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(res => res.json())
-      .then(req => {
-        setData(req.data);
-      });
-  }, []);
-
   // useEffect(() => {
-  //   fetch('/data/detail/detail.json')
+  //   fetch(`http://localhost:8000/product/${params.id}`, {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   })
   //     .then(res => res.json())
   //     .then(req => {
   //       setData(req.data);
   //     });
   // }, []);
+
+  useEffect(() => {
+    fetch('/data/detail/detail.json')
+      .then(res => res.json())
+      .then(req => {
+        setData(req.data);
+      });
+  }, []);
 
   return (
     <div className={css.detail}>
