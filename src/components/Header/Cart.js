ea1ed8ad4af5dc383a2245a15edbdb7101c42b1d
Yim Ji Young <87253461+Roxy100@users.noreply.github.com>
2022-09-07 15:47:11 +0900

    [Add]: 장바구니 담는 수량만큼 Header의 장바구니 수량변화 나타내기 (#52)

--- src/components/Header/Cart.js
+++ src/components/Header/Cart.js
@@ -1,14 +1,31 @@
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import css from './Cart.module.scss';
 
 function Cart() {
   const navigate = useNavigate();
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/carts', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+        setCartCount(data.data.cartList.length);
+      });
+  }, []);
+
   return (
     <div className={css['cart-box']} onClick={() => navigate('/cart')}>
       <img src="/image/header/cart.png" alt="cart" />
       <span>장바구니</span>
       <div>
-        <span>0</span>
+        <span>{cartCount}</span>
       </div>
     </div>
   );
