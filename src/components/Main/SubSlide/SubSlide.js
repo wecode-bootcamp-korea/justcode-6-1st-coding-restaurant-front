1035437537af0978fab2d5f15362ecc67782cb8f
poylib <poy_lib@naver.com>
2022-09-10 16:20:50 +0900

    [Fix]: 서브 캐러셀 더보기 버튼 간편요리에만 뜨도록 변경 및 캐러셀 버그 수정, 카드 누르면 이동 추가 (#71)
    * [Fix] 서브 캐러셀 더보기 버튼 간편요리에만 뜨도록 변경, 캐러셀 기능 수정
    
    * [Fix]: 버튼 위치 수정

--- src/components/Main/SubSlide/SubSlide.js
+++ src/components/Main/SubSlide/SubSlide.js
@@ -1,42 +1,43 @@
-import React, { useState, useEffect, Children } from 'react';
-import { Link, useSearchParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import css from './SubSlide.module.scss';
 function SubSlide(props) {
   const [imgData, setImgData] = useState([]);
   const [imgLocation, setImgLocation] = useState(0);
-
+  const navigate = useNavigate();
+  console.log(props.query);
   /////////////목데이터////////////
 
-  // useEffect(() => {
-  //   fetch(`/data/main/${props.query}.json`)
-  //     .then(res => res.json())
-  //     .then(res => setImgData(res.data));
-  // }, []);
+  useEffect(() => {
+    fetch(`/data/main/${props.query}.json`)
+      .then(res => res.json())
+      .then(res => setImgData(res.data));
+  }, []);
 
   ///////서버통신///////////
-  useEffect(() => {
-    fetch(`http://localhost:8000/products?${props.query}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(res => res.json())
-      .then(res => {
-        setImgData(res.data);
-        // console.log('민우님 호출');
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch(`http://localhost:8000/products?${props.query}`, {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   })
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       setImgData(res.data);
+  //       // console.log('민우님 호출');
+  //     });
+  // }, []);
 
   const leftBtn = () => {
     if (imgLocation > 0) {
-      setImgLocation(imgLocation - 1);
+      setImgLocation(imgLocation - 2);
     }
   };
 
   const rightBtn = () => {
     if (imgLocation < 1) {
-      setImgLocation(imgLocation + 1);
+      setImgLocation(imgLocation + 2);
     }
   };
   return (
@@ -44,23 +45,29 @@ function SubSlide(props) {
       <div className={css.title}>
         <div>{props.children}</div>
         <div className={css.more}>
-          <Link to="/products?category=mealKit&orderBy=viewCount&page=1&pageSize=6">
-            더보기
-          </Link>
-          <span>&gt;</span>
+          {props.query === 'category=mealKit' && (
+            <Link to="/products?category=mealKit&orderBy=viewCount&page=1&pageSize=6">
+              더보기
+              <span>&gt;</span>
+            </Link>
+          )}
         </div>
       </div>
       <div className={css['img-slide']}>
         <div
           className={css.cardlist}
           style={{
-            transform: `translate3d(${imgLocation * -80 + 'vw'},0px,0px)`,
+            transform: `translate3d(${imgLocation * -50 + '%'},0px,0px)`,
             transition: '450ms',
           }}
         >
           {imgData.map(data => {
             return (
-              <div key={data.id} className={css.card}>
+              <div
+                key={data.id}
+                className={css.card}
+                onClick={() => navigate(`/product/${data.id}`)}
+              >
                 <img src={data['image_thumbnail']} />
                 <div className={css.content}>
                   <h2>{data.productor}</h2>
