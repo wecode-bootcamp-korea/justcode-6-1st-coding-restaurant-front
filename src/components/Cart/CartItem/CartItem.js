61058e845b6748727bf573476057697fd364c4b1
YesunPark <108933466+YesunPark@users.noreply.github.com>
2022-11-11 11:45:36 +0900

    [Refactor]: 장바구니 페이지 리펙토링 (#83)
    * [Merge]: 메인 병합
    
    * [Refactor]: 불필요한 공백, fragment 삭제
    
    * [Refactor: 필요없는 props, react import, 선언 삭제
    
    * [Refactor]: state로 관리할 필요없는 데이터 삭제
    
    * [Delete]: 데이터 보낸 후 콘솔확인 용 코드 삭제
    
    * [Refactor]: &&연산자 두번 사용한 것 삼항연산자로 대체
    
    * [Refactor]: 필요없는 fragment 삭제
    
    * [Refactor]: self-closing 가능한 요소 수정
    
    * [Refactor]: ===연산자 사용, a태그 Link로 변경
    
    * [Refactor]: import 순서 컨벤션에 맞게 수정

--- src/components/Cart/CartItem/CartItem.js
+++ src/components/Cart/CartItem/CartItem.js
@@ -1,25 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import css from './CartItem.module.scss';
 
-function CartItem({
-  item,
-  cartList,
-  itemState,
-  setItemState,
-  cartCount,
-  setCartCount,
-}) {
-  const { id, brandName, itemName, img, options } = item;
+function CartItem({ item, itemState, setItemState, cartCount, setCartCount }) {
+  const { brandName, itemName, img, options } = item;
   const [count, setCount] = useState(item.quantity);
-  const [price, setPrice] = useState(item.price);
   const [itemTotalPrice, setItemTotalPrice] = useState(
-    Number(`${count * price}`)
+    Number(`${count * item.price}`)
   );
-
   const deliveryFee = 3500;
   const deliveryDate = '2022-09-06';
-
   const freeDelivery =
     itemTotalPrice >= 50000 ? '무료배송' : deliveryFee.toLocaleString() + ' 원';
 
@@ -33,19 +24,13 @@ function CartItem({
       body: JSON.stringify({
         cartsId: item.id,
       }),
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-      });
-
-    itemState == true && setItemState(false);
-    itemState == false && setItemState(true);
+    }).then(res => res.json());
+    itemState === true ? setItemState(false) : setItemState(true);
     setCartCount(cartCount - 1);
   };
 
   const countPlus = () => {
-    let newPrice = Number(`${(count + 1) * price}`);
+    let newPrice = Number(`${(count + 1) * item.price}`);
     setCount(count + 1);
     setItemTotalPrice(newPrice);
     fetch('http://localhost:8000/carts', {
@@ -58,18 +43,13 @@ function CartItem({
         cartsId: item.id,
         quantity: 1,
       }),
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-      });
-    itemState == true && setItemState(false);
-    itemState == false && setItemState(true);
+    }).then(res => res.json());
+    itemState === true ? setItemState(false) : setItemState(true);
   };
 
   const countMinus = () => {
     count >= 2 && setCount(count - 1);
-    count >= 2 && setItemTotalPrice(Number(`${(count - 1) * price}`));
+    count >= 2 && setItemTotalPrice(Number(`${(count - 1) * item.price}`));
     count >= 2 &&
       fetch('http://localhost:8000/carts', {
         method: 'PATCH',
@@ -81,69 +61,66 @@ function CartItem({
           cartsId: item.id,
           quantity: -1,
         }),
-      })
-        .then(res => res.json())
-        .then(data => {
-          console.log(data);
-        });
-    itemState == true && setItemState(false);
-    itemState == false && setItemState(true);
+      }).then(res => res.json());
+    itemState === true ? setItemState(false) : setItemState(true);
   };
 
   return (
-    <>
-      <div className={css['cart-item']}>
-        <div className={css['item-brand']}>
-          {brandName} ( 50,000 원 이상 무료 배송 / 현재{' '}
-          {itemTotalPrice.toLocaleString()} 원 )
-        </div>
-        <div className={css['item-body']}>
-          <img className={css['item-img']} alt={itemName} src={img} />
-          <div className={css['item-detail']}>
-            <div className={css['item-name']}>
-              <a>{itemName}</a>
+    <div className={css['cart-item']}>
+      <div className={css['item-brand']}>
+        {brandName} ( 50,000 원 이상 무료 배송 / 현재{' '}
+        {itemTotalPrice.toLocaleString()} 원 )
+      </div>
+      <div className={css['item-body']}>
+        <img className={css['item-img']} alt={itemName} src={img} />
+        <div className={css['item-detail']}>
+          <div className={css['item-name']}>
+            <Link>{itemName}</Link>
+          </div>
+          <div className={css['item-detail-list']}>
+            <div className={css['item-select']}>
+              <input type="checkbox" />
+              <div className={css['item-option']}>{options}</div>
             </div>
-            <div className={css['item-detail-list']}>
-              <div className={css['item-select']}>
-                <input type="checkbox"></input>
-                <div className={css['item-option']}>{options}</div>
-              </div>
 
-              <div>
-                <span className={css.price}>{price.toLocaleString()} 원</span>
-                <span className={`${css.small} ${css.count}`}>
-                  <img
-                    onClick={countMinus}
-                    src=" https://cdn-icons-png.flaticon.com/128/992/992683.png"
-                    className={`${css.button} ${css.minus}`}
-                  />
-                  {count}
-                  <img
-                    onClick={countPlus}
-                    src="https://cdn-icons-png.flaticon.com/128/3303/3303893.png"
-                    className={`${css.button} ${css.plus}`}
-                  />
-                </span>
+            <div>
+              <span className={css.price}>
+                {item.price.toLocaleString()} 원
+              </span>
+              <span className={`${css.small} ${css.count}`}>
+                <img
+                  onClick={countMinus}
+                  src=" https://cdn-icons-png.flaticon.com/128/992/992683.png"
+                  alt="minusButton"
+                  className={`${css.button} ${css.minus}`}
+                />
+                {count}
+                <img
+                  onClick={countPlus}
+                  src="https://cdn-icons-png.flaticon.com/128/3303/3303893.png"
+                  alt="plusButton"
+                  className={`${css.button} ${css.plus}`}
+                />
+              </span>
 
-                <span className={css['total-price']}>
-                  {itemTotalPrice.toLocaleString()} 원
-                </span>
-                <span className={css['delivery-fee']}>{freeDelivery}</span>
+              <span className={css['total-price']}>
+                {itemTotalPrice.toLocaleString()} 원
+              </span>
+              <span className={css['delivery-fee']}>{freeDelivery}</span>
 
-                <span className={css['delivery-date']}>{deliveryDate}</span>
+              <span className={css['delivery-date']}>{deliveryDate}</span>
 
-                <button
-                  onClick={deleteClick}
-                  className={`${css.small} ${css.delete}`}
-                >
-                  x
-                </button>
-              </div>
+              <button
+                onClick={deleteClick}
+                className={`${css.small} ${css.delete}`}
+              >
+                x
+              </button>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
